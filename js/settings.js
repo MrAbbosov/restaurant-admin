40c4bbe579dd73c9481158f93bb6887b78bcba8d
Farrukhbek <68499298+1Farrukhbek@users.noreply.github.com>
2021-03-11 15:16:04 +0500

    Render new added products

--- js/settings.js
+++ js/settings.js
@@ -1,25 +1,93 @@
+let elNewProductForm = document.querySelector('.modal-form');
+let elDishesList = document.querySelector('.js-add-dishes-list');
+let elAddNewProductName = elNewProductForm.querySelector('.js-new-product-name');
+let elAddNewProductPrice = elNewProductForm.querySelector('.js-new-product-price');
+let elNewProductImg = elNewProductForm.querySelector('.js-new-product-img');
+let elNewProductPreview = elNewProductForm.querySelector('.js-new-product-preview');
+let elAddNewProductTextarea = elNewProductForm.querySelector('.js-item-input-textarea');
+let elAddNewProductBtn = elNewProductForm.querySelector('.js-add-new-product-btn');
 
-var modal = document.getElementById("myModal");
+let elTemplateNewProduct = document.querySelector('#add-new-dish-template').content;
 
-// Get the button that opens the modal
-var modalBtn = document.getElementById("myBtn");
+let modal = document.getElementById("myModal");
+let modalBtn = document.getElementById("myBtn");
+let span = document.getElementsByClassName("close")[0];
 
-// Get the <span> element that closes the modal
-var span = document.getElementsByClassName("close")[0];
+let $_ = function(selector, node = document) {
+  return node.querySelector(selector);
+}
 
-// When the user clicks the button, open the modal 
 modalBtn.onclick = function() {
   modal.style.display = "block";
 }
 
-// When the user clicks on <span> (x), close the modal
 span.onclick = function() {
   modal.style.display = "none";
 }
 
-// When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
   if (event.target == modal) {
     modal.style.display = "none";
   }
 }
+
+
+elNewProductImg.addEventListener("change", (evt)=> {
+  evt.preventDefault()
+
+  elNewProductPreview.src = evt.target.value
+})
+
+elNewProductForm.addEventListener("submit", async (evt)=> {
+  evt.preventDefault()
+  
+  try {
+    const newProduct = await fetch(`${CONFIG.HOST}/admin/product`, {
+      method: 'post',
+      headers: { 'Content-Type': 'application/json' },
+      body: JSON.stringify({
+        name: elAddNewProductName.value,
+        price: elAddNewProductPrice.value,
+        image: elNewProductImg.value,
+        info: elAddNewProductTextarea.value
+      })
+    })
+    await newProduct.json()
+
+    elNewProductForm.reset()
+    modal.style.display = "none";
+  } catch (error) {
+    console.log(error);
+  }
+})
+
+
+const fetchNewProductData = async (CONFIG) => {
+
+  const res = await fetch(`${CONFIG.HOST}/admin/products`)
+  const response = await res.json()
+
+
+  let listNewFragment = document.createDocumentFragment();
+
+
+  response.data.forEach((product) => {
+
+    let elProductItem = elTemplateNewProduct.cloneNode(true);
+
+
+    $_('.js-add-dish-img', elProductItem).src = product.product_image;
+    $_('.js-add-dish-name', elProductItem).textContent = product.product_name;
+    $_('.js-add-dish-price', elProductItem).textContent = product.product_price;
+    $_('.js-add-dish-btn', elProductItem).dataset.productId = product.product_id;
+  
+    listNewFragment.appendChild(elProductItem);
+  })
+
+  elDishesList.appendChild(listNewFragment);
+} 
+
+fetchNewProductData(CONFIG)
+
+
+
